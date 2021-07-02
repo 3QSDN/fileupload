75ca526e7afa2650d51357a12a1a7d766b04d668
Michael Ruge <michael.ruge@3q.video>
2021-07-02 19:31:53 +0200

    js: upload location call from server component to hide SDN API Key for clients

---
+++ js/server.php
@@ -0,0 +1,36 @@
+<?php
+
+$ProjectId = YourProjectID;
+$APIKey = "Your API Key";
+$APIURI = "https://sdn-dev.3qsdn.com/api/v2/projects/{$ProjectId}/files";
+$Location = "";
+
+$ch = curl_init($APIURI);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "X-AUTH-APIKEY: $APIKey",
+    "Content-Type: application/json; charset=utf-8",
+    "Accept: application/json; charset=utf-8"
+));
+
+// pass the json Body to SDN API
+$inputJSON = file_get_contents('php://input');
+curl_setopt($ch, CURLOPT_POSTFIELDS, $inputJSON);
+
+// extract location header value
+curl_setopt($ch, CURLOPT_HEADERFUNCTION,
+    function($curl, $header) use (&$Location) {
+        $len = strlen($header);
+        $header = explode(':', $header, 2);
+
+        // Location header value
+        if (strtolower(trim($header[0])) == "location") $Location = trim($header[1]);
+
+        return $len;
+    }
+);
+$resultstr = curl_exec($ch);
+
+// return upload location to js
+echo $Location;
